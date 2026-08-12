835198016e4cc4a07526e435ff4766e23f427507
henriquerm1995 <henriquerm1995@gmail.com>
2026-08-12 21:40:50 +0100

    Add employee model and admin integration

---
+++ employees/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+
+class Employee(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="employee",
+    )
+# Create your models here.
+    cpf = models.CharField(max_length=14, unique=True)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
